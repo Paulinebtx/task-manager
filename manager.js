afbfcfe0811ecf56900c367a97a60135ec6c0cac
pauline_btx <boiteux.pauline@gmail.com>
2021-05-18 16:41:48 +0200

    add some changes

--- manager.js
+++ manager.js
@@ -1,41 +1,49 @@
-const readline = require('readline'); 
+const readline = require('readline');
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
-let ListAllTask = ["Creat a function", "Find the algorithm"]
+let ListAllTask = ['Creat a function', 'Find the algorithm'];
 
-let a = "Welcome to your task manager, Press:";
-let b = "1. to see all your tasks";
-let c = "2. to add a task";
-let d = "3. to delete a task";
-let e = "4. to mark a task as done";
-let f = "5. to Exit the task manager";
+let a = 'Welcome to your task manager, Press:';
+let b = '1. to see all your tasks';
+let c = '2. to add a task';
+let d = '3. to delete a task';
+let e = '4. to mark a task as done';
+let f = '5. to Exit the task manager';
 
 const menu = () => {
+  console.log(a);
+  console.log(b);
+  console.log(c);
+  console.log(d);
+  console.log(e);
+  console.log(f);
 
-    console.log(a)
-    console.log(b)
-    console.log(c)
-    console.log(d)
-    console.log(e)
-    console.log(f)
+  rl.question('Enter your number of choice ', (nbr) => {
+    if (nbr === '1') {
+      console.log(ListAllTask);
+      menu();
+    } else if (nbr === '2') {
+      rl.question('Which task do you want to add?', (answer) => {
+        ListAllTask.push(answer);
+        console.log('Your task has been added on the list');
+        menu();
+      });
+    } else if (nbr === '3') {
+      rl.question('Which task do you want to delete?', (answer) => {
+        removetask(answer);
+        console.log('Your task has been deleted on list');
+        menu();
+      });
+    } else if (nbr === '5') {
+      console.log('Exit');
+      rl.close();
+    }
+  });
+};
 
-    rl.question("Enter your number of choice ", nbr => {
-        if(nbr === "1") {
-            console.log(ListAllTask)
-            rl.close()
-        }else if (nbr === "2"){
-            rl.question("Which task do you want do add?", answer => {
-                ListAllTask.push(answer)
-                console.log("Your task has been added on the list")
-                rl.close()
-                
-            })
-        }
-    
-    
-    
-})
-menu(); 
+
+
+menu()
 
